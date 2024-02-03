b0e05f3e48df25c960705c7390c860f0f1730736
dillonzellis <dillonzellis@gmail.com>
2024-02-02 21:10:42 -0500

    add neovim

---
+++ .config/nvim/lua/plugins/colorshemes.lua
@@ -0,0 +1,14 @@
+return {
+  { "craftzdog/solarized-osaka.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rmehri01/onenord.nvim" },
+  { "Shatur/neovim-ayu" },
+  { "gbprod/nord.nvim" },
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+}
