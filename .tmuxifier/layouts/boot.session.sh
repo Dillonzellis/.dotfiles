25f5974250cc719a31399b747471365745e89604
dillonzellis <dillonzellis@gmail.com>
2024-02-02 21:01:55 -0500

    add tmux

---
+++ .tmuxifier/layouts/boot.session.sh
@@ -0,0 +1,18 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/Projects/boot/python/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "boot"; then
+
+	new_window "neovim"
+	split_v 20
+	run_cmd "npm run dev"
+	select_pane 0
+	run_cmd "nvim ."
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
