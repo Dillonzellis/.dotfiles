0b7c642606c125f62e2ce9c98688d22904e203fc
dillonzellis <dillonzellis@gmail.com>
2024-02-06 13:58:17 -0500

    feat: add new keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,17 @@
 
 -- replace word under cursor throughout file
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Move selected lines with shift+j or shift+k
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor centred while scrolling up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Delete to void register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Delete shift+q keymap
+vim.keymap.set("n", "Q", "<nop>")
