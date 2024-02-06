deb803988acf79f328f67c366cd6623c7e8aba5e
dillonzellis <dillonzellis@gmail.com>
2024-02-06 14:11:30 -0500

    fix: remove nvim notify

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,5 +4,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
   -- { "nvim-lualine/lualine.nvim", enabled = false },
 }
