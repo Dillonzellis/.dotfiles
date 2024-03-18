7649af86716f1bef518ce44cb53b337f12d1bff7
dillonzellis <dillonzellis@gmail.com>
2024-03-17 21:58:32 -0400

    fix: modular fix

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -41,13 +41,6 @@ require("lazy").setup({
 	require("zell/plugins/indent_line"),
 	require("zell/plugins/trouble"),
 	require("zell/plugins/vim-tmux-navigator"),
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-	-- { import = 'custom.plugins' },
 }, {
 	ui = {
 		-- If you have a Nerd Font, set icons to an empty table which will use the
@@ -69,5 +62,4 @@ require("lazy").setup({
 		},
 	},
 })
-
 -- vim: ts=2 sts=2 sw=2 et
